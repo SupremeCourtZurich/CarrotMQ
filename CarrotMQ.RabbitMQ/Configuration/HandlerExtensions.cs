5b8ec39806ebe4f9d1dd5b573b8133c096ced418
guido-frerker <179205828+guido-frerker@users.noreply.github.com>
2026-02-05 07:26:58 +0100

    Allow BindTo to use DirectExchange/TopicExchange instead of their respective derived builder types (#23)
    - rename BindingConfigurationBuilderExtensions to HandlerExtensions
    - allow BindTo to use DirectExchange/TopicExchange instead of their respective derived builder types (DirectExchangeBuilder/TopicExchangeBuilder)

---
+++ CarrotMQ.RabbitMQ/Configuration/HandlerExtensions.cs
@@ -0,0 +1,34 @@
+﻿using CarrotMQ.Core.Configuration;
+using CarrotMQ.Core.Dto.Internals;
+using CarrotMQ.RabbitMQ.Configuration.Exchanges;
+using CarrotMQ.RabbitMQ.Configuration.Queues;
+
+namespace CarrotMQ.RabbitMQ.Configuration;
+
+/// <summary>
+/// Provides extensions for configuring the bindings between exchanges and queues.
+/// </summary>
+public static class HandlerExtensions
+{
+    /// <inheritdoc cref="Handler{TMessage,TResponse}" />
+    extension<TMessage, TResponse>(Handler<TMessage, TResponse> handler) where TMessage : _IMessage<TMessage, TResponse> where TResponse : class
+    {
+        /// <summary>
+        /// Bind the given <paramref name="exchange" /> to the given <paramref name="queue" /> using the <typeparamref name="TMessage" /> as
+        /// routing key
+        /// </summary>
+        public Handler<TMessage, TResponse> BindTo(DirectExchange exchange, Queue queue)
+        {
+            return handler.BindTo(exchange.Name, queue.QueueName);
+        }
+
+        /// <summary>
+        /// Bind the given <paramref name="exchange" /> to the given <paramref name="queue" /> using the <typeparamref name="TMessage" /> as
+        /// routing key
+        /// </summary>
+        public Handler<TMessage, TResponse> BindTo(TopicExchange exchange, Queue queue)
+        {
+            return handler.BindTo(exchange.Name, queue.QueueName);
+        }
+    }
+}
